fdcbb075d589ffc39bbc3870a36371f8bf4d8d63
RobertArustamyan <robertarustamyan2@gmail.com>
2025-12-09 23:31:35 +0100

    Update linear_cost.py
    * add implementation of gradient method

--- algorithms/transactions/linear_cost.py
+++ algorithms/transactions/linear_cost.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 
 from algorithms.transactions.cost import Costs
@@ -10,4 +12,12 @@ class LinearCost(Costs):
     def compute_cost(self, prev_weights: np.ndarray, new_weights: np.ndarray) -> float:
         turnover = np.sum(np.abs(new_weights - prev_weights))
         cost = self.gamma * turnover
+
         return cost
+
+    def gradient(self, prev_weights: np.ndarray, new_weights: np.ndarray) -> np.ndarray:
+        diff = new_weights - prev_weights
+        sign = np.sign(diff)
+        sign[np.isclose(diff, 0.0, atol=1e-12)] = 0.0
+
+        return self.gamma * sign
