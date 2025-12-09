af6d0baf24d57cd1c6110e4c0854bcbb7a8d78de
RobertArustamyan <robertarustamyan2@gmail.com>
2025-12-09 23:30:07 +0100

    Add gradient to abstract cost class

--- algorithms/transactions/cost.py
+++ algorithms/transactions/cost.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+import numpy as np
+
 
 class Costs(ABC):
     def __init__(self):
@@ -6,4 +10,20 @@ class Costs(ABC):
 
     @abstractmethod
     def compute_cost(self, prev_weights, new_weights) -> float:
-        pass
+        raise NotImplementedError
+
+    @abstractmethod
+    def gradient(self, prev_weights, new_weights) -> List[float]:
+        raise NotImplementedError
+
+    def numerical_gradient(self, prev_weights, new_weights, eps=1e-6) -> List[float]:
+        g = np.zeros_like(new_weights, dtype=float)
+        for i in range(len(new_weights)):
+            p_plus = new_weights.copy()
+            p_minus = new_weights.copy()
+            p_plus[i] += eps
+            p_minus[i] -= eps
+            c_plus = self.compute_cost(prev_weights, p_plus)
+            c_minus = self.compute_cost(prev_weights, p_minus)
+            g[i] = (c_plus - c_minus) / (2 * eps)
+        return g
